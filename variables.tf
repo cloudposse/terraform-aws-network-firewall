bce3bf02bfef2fa83c6dc5035d1a91d41733d178
aknysh <andriy.knysh@gmail.com>
2022-10-25 23:11:35 -0400

    Init

--- variables.tf
+++ variables.tf
@@ -78,12 +78,12 @@ variable "subnet_change_protection" {
 }
 
 variable "rule_group_config" {
-  type        = map(any)
+  type        = any
   description = "Rule group configuration. Refer to https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/networkfirewall_rule_group for configuration details"
 }
 
 variable "logging_config" {
-  type        = map(any)
+  type        = any
   description = "Logging configuration"
   default     = {}
 }
