aa1330432f35362c8731b02f680deece4429fe80
aknysh <andriy.knysh@gmail.com>
2025-10-22 20:36:56 -0400

    add TWG integration

--- versions.tf
+++ versions.tf
@@ -3,8 +3,10 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
+      source = "hashicorp/aws"
+      # The `transit_gateway_id` parameter has been released in AWS provider version 6.5.0 (released July 24, 2025).
+      # https://github.com/hashicorp/terraform-provider-aws/blob/v6.5.0/CHANGELOG.md
+      version = ">= 6.5.0"
     }
   }
 }
